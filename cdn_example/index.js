059c3b9b089593e0d510d16b76f26510e51b1033
Your Name <you@example.com>
2022-05-25 10:14:12 +0300

    hello

---
+++ cdn_example/index.js
@@ -0,0 +1,27 @@
+//ReactDOM.render(hello, document.getElementById('root'));
+
+const element = 
+
+        <ul>
+            <li>
+            <h1>I'm learning ReactDOM</h1>
+            </li>
+            <li>
+            <h2>ReactDOM.render elments, postiion</h2>
+            </li>
+        </ul>
+;
+
+ReactDOM.render(
+    
+    element
+    ,
+    document.querySelector('#root')
+);
+
+/*
+const h1 = document.createElement("h1")
+h1.textConeent = "I'm learning ReactDOM"
+h1.className = "header"
+document.getElementById("root").appendChild(h1)
+*/
